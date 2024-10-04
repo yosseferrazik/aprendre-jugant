f3d1125af6d95ceec37a9d1987ab4d423e9f207a
yosseferrazik <yosseferrazik@gmail.com>
2024-10-04 09:38:39 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <iframe
+        src="https://www.edu365.cat/workshop/joc/lletres/index.html"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        allowFullScreen
+        title="Edu365 Game"
+      ></iframe>
+    </>
+  );
+}
+
+export default App;
